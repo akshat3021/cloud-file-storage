519a20464c322e9e87a3803ca2d5acd4bd072fa7
Akshat Aswal <akshataswal84@gmail.com>
2025-10-11 21:05:54 +0530

    feat: Create and display basic Dashboard component

---
+++ src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div>
+      <h1>Welcome to Your Dashboard</h1>
+      <p>This is where the user's files will be displayed.</p>
+    </div>
+  );
+}
+
+export default Dashboard;
